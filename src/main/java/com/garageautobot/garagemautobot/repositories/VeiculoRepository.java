907f0e558c2951a6877674a9a88ec80f3e2decd2
Gabriel Vezu <gabrielvezusonego1004@gmail.com>
2025-09-29 14:43:29 -0300

    Fix bug database

--- src/main/java/com/garageautobot/garagemautobot/repositories/VeiculoRepository.java
+++ src/main/java/com/garageautobot/garagemautobot/repositories/VeiculoRepository.java
@@ -1,13 +1,15 @@
 package com.garageautobot.garagemautobot.repositories;
 
 import com.garageautobot.garagemautobot.entities.Veiculo;
+import com.garageautobot.garagemautobot.entities.StatusVeiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
     List<Veiculo> findByClienteId(Long clienteId);
     List<Veiculo> findByPlacaContainingIgnoreCase(String placa);
+
+    List<Veiculo> findByStatusIn(List<StatusVeiculo> status);
 }
--- src/main/java/com/garageautobot/garagemautobot/services/VeiculoService.java
+++ src/main/java/com/garageautobot/garagemautobot/services/VeiculoService.java
@@ -1,9 +1,12 @@
 package com.garageautobot.garagemautobot.services;
 
+import com.garageautobot.garagemautobot.entities.StatusVeiculo;
 import com.garageautobot.garagemautobot.entities.Veiculo;
 import com.garageautobot.garagemautobot.repositories.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.garageautobot.garagemautobot.entities.StatusVeiculo;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -37,4 +40,11 @@ public class VeiculoService {
     public void delete(Long id) {
         veiculoRepository.deleteById(id);
     }
+    
+    public List<Veiculo> findByStatusEmManutencaoOuAguardando() {
+        return veiculoRepository.findByStatusIn(
+            List.of(StatusVeiculo.EM_MANUTENCAO, StatusVeiculo.AGUARDANDO_PECA)
+        );
+    }
+    
 }
